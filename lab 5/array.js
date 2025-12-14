8e14659e8705a2faebf515e222bd1ab6b5dcb127
fairskiy <kovrigad237@gmail.com>
2025-12-14 18:13:22 +0200

    Add files via upload

---
+++ lab 5/array.js
@@ -0,0 +1,22 @@
+const array = () => {
+  const arr = [];
+  const get = (index) => arr[index];
+  get.pop = () => arr.pop();
+  get.push = (x) => arr.push(x);
+  return get;
+};
+const arr = array();
+
+arr.push("first");
+arr.push("second");
+arr.push("third");
+
+console.log(arr(0));
+console.log(arr(1));
+console.log(arr(2));
+
+console.log(arr.pop());
+console.log(arr.pop());
+console.log(arr.pop());
+
+console.log(arr.pop());
